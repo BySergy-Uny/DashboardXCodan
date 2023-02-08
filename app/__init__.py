e5b22e91172038617b230bc5086e9b421a061e9f
BySergy-Uny <bysergy.uny@gmail.com>
2023-02-08 23:39:01 +0100

    Creada la pagina de inicio de la web

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 from dotenv import dotenv_values
 
 config = dotenv_values("./.env")
-app = Flask(__name__)
+app = Flask(__name__, 
+            static_url_path='', 
+            static_folder='static',
+            template_folder='templates')
+
+
+from app.routes import landing
