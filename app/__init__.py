285e4a0ebb4edd77c6fa6a1d9d4934950db67a79
BySergy-Uny <bysergy.uny@gmail.com>
2023-02-09 18:09:20 +0100

    Conexion de la base de datos

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from dotenv import dotenv_values
+from app.tools.database_connection import *
 
 config = dotenv_values("./.env")
+
+
+db_mongo = databaseTool(config)
+
 app = Flask(__name__, 
             static_url_path='', 
             static_folder='static',
@@ -9,3 +14,4 @@ app = Flask(__name__,
 
 
 from app.routes import landing
+
