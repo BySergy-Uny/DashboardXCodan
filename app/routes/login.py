5c2cfa072ff8ec7562b5039c8866847bfd46469f
BySergy-Uny <bysergy.uny@gmail.com>
2023-02-11 20:41:16 +0100

    Creado el Form en Flask

--- app/routes/login.py
+++ app/routes/login.py
@@ -1,8 +1,17 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 
 @app.route("/login")
 def login():
     return render_template('login.html')
+
+
+@app.route('/login-form', methods=['POST'])
+def login_form():
+    data = request.form
+    return {
+        'username'     : data['username'],
+        'password' : data['password'],
+    }
 
