8dbcbfd1fab70c95ea932d6fe20d23535c2917f2
BySergy-Uny <bysergy.uny@gmail.com>
2023-07-06 01:09:50 +0200

    Añadida la seguridad

--- app/routes/history.py
+++ app/routes/history.py
@@ -1,6 +1,6 @@
-from app import app
+from app import app, login_manager
 from flask import render_template, redirect, url_for
-
+from flask_login import login_required
 import pandas as pd
 from app.tools.detaApp import *
 
@@ -9,6 +9,7 @@ import plotly.io as px_translate
 import plotly.figure_factory as ff
 
 @app.route("/history")
+@login_required
 def history():
     response = dataDrive.get("reg_nodes_measures.csv")
     df = pd.read_csv(response)
@@ -19,3 +20,10 @@ def history():
     graph = px_translate.to_html(px.line(df,title='Temperatura', y='temperatura', render_mode='svg'))
     table = px_translate.to_html(ff.create_table(df[['date', 'tvoc', 'eco2', 'humedad', 'temperatura']].tail()))
     return render_template('history.html', graph=graph, table=table)
+
+
+@login_manager.unauthorized_handler     # In unauthorized_handler we have a callback URL 
+def unauthorized_callback():            # In call back url we can specify where we want to 
+       return redirect(url_for('login')) # redirect the user in my case it is login page!
+
+
